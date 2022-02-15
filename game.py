67c009ad0d88e2f8be43016c7ac6149222306386
ZachHofmeister <zachhof@me.com>
2022-02-14 16:30:40 -0800

    New ship graphics

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (150, 150, 150)
+        self.bg_color = (50, 50, 50)
 
         self.ship_speed_factor = 3
 
